800daf860cc1961510176692d6f253efda286101
jenifer9 <jeni9jacinto7p@gmail.com>
2022-09-19 23:15:12 -0600

    Agregamos el archivo index.html

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola mundo!"
+    return render_template('index.html')
 
 #debug=True es para mantener el servidor reiniciando 
 #despues de los cambios mientras aun esta en desarrollo
 if __name__=='__main__':
     app.run(debug=True)
-
